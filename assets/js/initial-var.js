f0dbac635fe5ec43f766e3f01d0d4fe6cce3ac81
laukkusehat <laukkusehat@gmail.com>
2024-06-18 08:59:12 +0700

    fix

--- assets/js/initial-var.js
+++ assets/js/initial-var.js
@@ -1,1 +1,1 @@
-var script_url = "https://script.google.com/macros/s/AKfycbxM3_7EOYm9dBsERI_nT10UlCf7pVEVgxMDm4zXAyh49o-3r9G8z31-IP7XnpwhR_t5BQ/exec";
+var script_url = "https://script.google.com/macros/s/AKfycbxYX9aVCoqYNZi2_IFbqgngb8SEoLYkSLQ-qxZxwjxaSI8gS__zKbRAPnPGRWAkfXsi3A/exec";
